5d6893601feca81f5ac3b9439516f9c66f917c87
Arry <arifquerry@gmail.com>
2026-01-12 02:31:09 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.log("Server error");
+            console.log(err);
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        })
+    })
+    .catch((err) => {
+        console.log("DB connection failed");
+        console.log(err);
+    });
