53053d7dfa36b20245ee5d8d55d7c6ece1e8f069
Arry <arifquerry@gmail.com>
2026-01-15 00:01:33 +0530

    fix: format code and add missing semicolons in controller files

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,41 +1,40 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
+    const { name, description } = req.body;
 
     //TODO: create playlist
-})
+});
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
+    const { userId } = req.params;
     //TODO: get user playlists
-})
+});
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
+    const { playlistId } = req.params;
     //TODO: get playlist by id
-})
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-})
+    const { playlistId, videoId } = req.params;
+});
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
+    const { playlistId, videoId } = req.params;
     // TODO: remove video from playlist
-
-})
+});
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
+    const { playlistId } = req.params;
     // TODO: delete playlist
-})
+});
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
+    const { playlistId } = req.params;
+    const { name, description } = req.body;
     //TODO: update playlist
-})
+});
 
 export {
     createPlaylist,
@@ -44,5 +43,5 @@ export {
     addVideoToPlaylist,
     removeVideoFromPlaylist,
     deletePlaylist,
-    updatePlaylist
-}
+    updatePlaylist,
+};
