26535edcbed696a1e0b442821916d657b6161667
Arry <arifquerry@gmail.com>
2026-01-14 18:19:10 +0530

    feat: add additional user routes for password change, account updates, and profile retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
     logoutUser,
     registerUser,
     refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImg,
+    getUserChannelProfile,
+    getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { veryfyJWT } from "../middlewares/auth.middleware.js";
@@ -19,10 +26,19 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
-
 // secured route
 router.route("/logout").post(veryfyJWT, logoutUser);
-
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(veryfyJWT, changeCurrentPassword);
+router.route("/current-user").get(veryfyJWT, getCurrentUser);
+router.route("/update-account").patch(veryfyJWT, updateAccountDetails);
+router
+    .route("/update-avatar")
+    .patch(veryfyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/update-cover-img")
+    .patch(veryfyJWT, upload.single("cov erImg"), updateUserCoverImg);
+router.route("/c/:username").get(veryfyJWT, getUserChannelProfile);
+router.route("/watch-history").get(veryfyJWT, getWatchHistory);
 
 export default router;
