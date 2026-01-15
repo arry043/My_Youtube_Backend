cc803a0d1debe857c9b33cfc48ef1638fbb4abd8
Arry <arifquerry@gmail.com>
2026-01-15 11:11:58 +0530

    feat: implement healthcheck endpoint and enhance tweet creation logic with error handling

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,7 +1,37 @@
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const { content } = req.body;
+
+    if(!content) {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const user = req?.user;
+
+    if(!user) {
+        throw new ApiError(401, "User is not authenticated");
+    }
+
+    const owner = await User.findById(user?._id).select(
+        "-password -refreshToken -watchHistory -coverImg -email -createdAt -updatedAt"
+    )
+
+    const tweet = await Tweet.create({
+        content,
+        owner: owner
+    })
+
+    await tweet.save();
+
+    return res
+    .status(200)
+    .json(new ApiResponse(201, tweet, "Tweet created successfully"));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
