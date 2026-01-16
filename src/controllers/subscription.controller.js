c9e6e0bedf845ef30ba1a75c0ab7ba308d711b02
Arry <arifquerry@gmail.com>
2026-01-16 11:02:07 +0530

    feat: implement toggleSubscription function to manage user subscriptions

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,8 +1,48 @@
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const { channelId } = req.params;
     // TODO: toggle subscription
+    const { channelId } = req.params;
+    const userId = req.user?._id;
+
+    if (!channelId) {
+        throw new ApiError(400, "Invailid request for subscribe the channel");
+    }
+
+    // check if already subscribed
+    const existingSubscription = await Subscription.findOne({
+        subscriber: userId,
+        channel: channelId,
+    });
+
+    if (existingSubscription) {
+        // already subscribed → unsubscribe
+        const result = await Subscription.deleteOne(
+            { _id: existingSubscription._id },
+            { new: true }
+        );
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, result, "Unsubscribed successfully"));
+    }
+
+    // not subscribed → subscribe
+    const subscription = await Subscription.create(
+        {
+            subscriber: userId,
+            channel: channelId,
+        },
+
+        { new: true }
+    );
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, subscription, "Subscribed successfully"));
 });
 
 // controller to return subscriber list of a channel
