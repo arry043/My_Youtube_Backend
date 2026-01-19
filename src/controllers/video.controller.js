cac3d4049676f9b9fe62633f36dda2bf2ab39551
Arry <arifquerry@gmail.com>
2026-01-19 10:01:40 +0530

    publish_video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -165,6 +165,21 @@ const deleteVideo = asyncHandler(async (req, res) => {
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
+    //TODO: toggle publish status
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found or deleted");
+    }
+
+    video.isPublished = !video.isPublished;
+    const updatedVideo = await video.save({
+        validateBeforeSave: false,
+        new: true,
+    });
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedVideo, "Video updated successfully"));
 });
 
 export {
