e43f0ad4a946ba97a073ffd4da9ad2bee246a5fc
Arry <arifquerry@gmail.com>
2026-01-19 09:59:07 +0530

    delete

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -152,6 +152,15 @@ const updateVideo = asyncHandler(async (req, res) => {
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
     //TODO: delete video
+    const video = await Video.findByIdAndDelete(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found or deleted");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Video deleted successfully"));
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
