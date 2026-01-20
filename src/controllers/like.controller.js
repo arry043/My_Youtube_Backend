867dde41695a73c5dfec1ed395867b972c3931a8
Arry <arifquerry@gmail.com>
2026-01-20 10:15:00 +0530

    sqa

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,6 +3,21 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
     //TODO: toggle like on video
+    if(!videoId) {
+        throw new ApiError(404, "Video not found or deleted");
+    }
+    const video = await Video.findById(videoId);
+    if(!video) {
+        throw new ApiError(404, "Video not found or deleted");
+    }
+    video.likes += 1;
+    const updatedVideo = await video.save({
+        validateBeforeSave: false,
+        new: true,
+    });
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedVideo, "Video liked successfully"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
